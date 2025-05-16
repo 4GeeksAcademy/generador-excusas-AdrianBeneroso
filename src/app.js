281a293ef035d9c338065bc9536ee9d3cee88ebe
AdrianBene <adrianbeneroso@hotmail.com>
2025-05-16 10:07:23 +0000

    limpieza codigo

--- src/app.js
+++ src/app.js
@@ -37,38 +37,46 @@ const textarea = document.getElementById('historial-excusas');
 const imprimir_excusa = document.getElementById('excuse-save');
 
 
-function actualizarHistorial(){
- textarea.value = historial_excusas.join('\n');
+function actualizarHistorial() {
+  textarea.value = historial_excusas.join('\n');
+}
+function numAleatorioArray(posicion) { // he añadido una funcion
+  return Math.floor(Math.random() * who.length);
 }
 
 
 document.getElementById('generar-excusa').addEventListener('click', function () {
 
 
+//MI CÓDIGO ANTIGUO
 
+  // let who_random = Math.floor(Math.random() * who.length);
+  // let action_random = Math.floor(Math.random() * action.length);
+  // let what_random = Math.floor(Math.random() * what.length);
+  // let when_random = Math.floor(Math.random() * when.length);
 
-  let who_random = Math.floor(Math.random() * who.length);
-  let action_random = Math.floor(Math.random() * action.length);
-  let what_random = Math.floor(Math.random() * what.length);
-  let when_random = Math.floor(Math.random() * when.length);
+  let who_random = numAleatorioArray(who.length); // estoy es lo que he modificado
+  let what_random = numAleatorioArray(what.length);// estoy es lo que he modificado
+  let action_random = numAleatorioArray(action.length);// estoy es lo que he modificado
+  let when_random = numAleatorioArray(when.length);// estoy es lo que he modificado
   const excuse = `${who[who_random]} ${action[action_random]} ${what[what_random]} ${when[when_random]}`;
   historial_excusas.push(excuse);
   imprimir_excusa.innerHTML = excuse;
- actualizarHistorial();
+  actualizarHistorial();
 
 })
 
 
 
-document.getElementById('eliminar-excusa').addEventListener('click',function(){
-   
-  if(historial_excusas.length > 0){
+document.getElementById('eliminar-excusa').addEventListener('click', function () {
+
+  if (historial_excusas.length > 0) {
     historial_excusas.pop();
     actualizarHistorial();
-    
+
   }
- 
-  
- 
- 
+
+
+
+
 })
